928599e87c5af8173445e50aa842ce9ead10d937
andrecoelho2002 <andrecoelho@crowdkeep.com>
2023-04-18 18:25:20 +0100

    Import Menu File

--- ScheduleES/src/main/java/ImportMenu.java
+++ ScheduleES/src/main/java/ImportMenu.java
@@ -1,8 +1,12 @@
-
+import org.apache.commons.io.FileUtils;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ImportMenu extends JFrame {
     private JRadioButton localFileButton;
@@ -28,9 +32,30 @@ public class ImportMenu extends JFrame {
         okButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (localFileButton.isSelected()) {
-                    // TODO: Handle local file import
+                    JFileChooser fileChooser = new JFileChooser();
+                    int result = fileChooser.showOpenDialog(ImportMenu.this);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = fileChooser.getSelectedFile();
+                        // TODO: Handle selected local file
+                        JOptionPane.showMessageDialog(null, "Selected file: " + selectedFile.getAbsolutePath(), "File selected", JOptionPane.INFORMATION_MESSAGE);
+                    }
                 } else if (urlButton.isSelected()) {
-                    // TODO: Handle URL import
+                    String url = JOptionPane.showInputDialog(null, "Enter URL to import from:", "Import from URL", JOptionPane.PLAIN_MESSAGE);
+                    if (url != null && !url.isEmpty()) {
+                        try {
+                            URL downloadUrl = new URL(url);
+                            String fileName = downloadUrl.getFile();
+                            fileName = fileName.substring(fileName.lastIndexOf('/') + 1);
+                            File outputFile = new File(fileName);
+                            FileUtils.copyURLToFile(downloadUrl, outputFile);
+                            // TODO: Handle downloaded file
+                            JOptionPane.showMessageDialog(null, "File downloaded to: " + outputFile.getAbsolutePath(), "Download complete", JOptionPane.INFORMATION_MESSAGE);
+                        } catch (MalformedURLException ex) {
+                            JOptionPane.showMessageDialog(null, "Invalid URL", "Error", JOptionPane.ERROR_MESSAGE);
+                        } catch (IOException ex) {
+                            JOptionPane.showMessageDialog(null, "Error downloading file", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
                 } else {
                     JOptionPane.showMessageDialog(null, "Please select an import option", "Error", JOptionPane.ERROR_MESSAGE);
                 }

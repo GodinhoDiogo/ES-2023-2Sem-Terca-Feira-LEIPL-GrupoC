fb7859cd2a157c3e579343a8ffc0a188f390771a
andrecoelho2002 <andrecoelho@crowdkeep.com>
2023-05-07 20:05:37 +0100

    fix

--- src/main/java/modules/ScheduleList.java
+++ src/main/java/modules/ScheduleList.java
@@ -2,6 +2,7 @@ package modules;
 
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -26,44 +27,44 @@ public class ScheduleList {
 	public List<Schedule> getSchedules() {
 		return schedules;
 	}
-public static ScheduleList fromWebcalString(String webcalData) {
-		
+	public static ScheduleList fromWebcalString(String webcalData) {
 	    ScheduleList schedules = new ScheduleList();
 	    ICalendar ical = Biweekly.parse(webcalData).first();
 	    System.out.println("this is gonna run");
 	    for (VEvent event : ical.getEvents()) {
-	    	String docente = "";
-	    	String description = event.getDescription().getValue();
-	    	String summary = event.getSummary().getValue();
-	    	if(!summary.contains("Alunos") || !summary.contains("Exame")) {
-	    		String docenteTag = "Docente: "; // The tag that appears before the docent value
-	    		int docenteTagIndex = description.indexOf(docenteTag); // Find the index of the docente tag
-	    		int docenteStartIndex = docenteTagIndex + docenteTag.length(); // Calculate the starting index of the docente value
-	    		int docenteEndIndex = description.indexOf('\n', docenteStartIndex); // Find the index of the end of the docente value
-	    		docente = description.substring(docenteStartIndex, docenteEndIndex).trim(); // Extract the docente value and remove any leading or trailing whitespace
-	    	}  	
-	    	System.out.println("--------------------------");
-	    	//System.out.println(description);
-	    	Pattern pattern = Pattern.compile("Shift:\\s+(\\w+)");
-	    	String turno = "";
-	         // Use a Matcher to find the first occurrence of the pattern in the text
-	         Matcher matcher = pattern.matcher(description);
-	         if (matcher.find()) {
-	             turno = matcher.group(1);
-	             
-	         }
-	        String date = new SimpleDateFormat("yyyy-MM-dd").format(event.getDateStart().getValue());
-	        String dayOfTheWeek = LocalDate.parse(date).getDayOfWeek().toString();
-	        String begin = new SimpleDateFormat("HH:mm").format(event.getDateStart().getValue());
-	        String end = new SimpleDateFormat("HH:mm").format(event.getDateEnd().getValue());
+	        String docente = "";
+	        String description = event.getDescription().getValue();
+	        String summary = event.getSummary().getValue();
+	        if(!summary.contains("Alunos") || !summary.contains("Exame")) {
+	            String docenteTag = "Docente: "; // The tag that appears before the docent value
+	            int docenteTagIndex = description.indexOf(docenteTag); // Find the index of the docente tag
+	            int docenteStartIndex = docenteTagIndex + docenteTag.length(); // Calculate the starting index of the docente value
+	            int docenteEndIndex = description.indexOf('\n', docenteStartIndex); // Find the index of the end of the docente value
+	            docente = description.substring(docenteStartIndex, docenteEndIndex).trim(); // Extract the docente value and remove any leading or trailing whitespace
+	        }
+	        System.out.println("--------------------------");
+	        //System.out.println(description);
+	        Pattern pattern = Pattern.compile("Shift:\\s+(\\w+)");
+	        String turno = "";
+	        // Use a Matcher to find the first occurrence of the pattern in the text
+	        Matcher matcher = pattern.matcher(description);
+	        if (matcher.find()) {
+	            turno = matcher.group(1);
+
+	        }
+	        String date = new SimpleDateFormat("dd/MM/yyyy").format(event.getDateStart().getValue());
+	        String dayOfTheWeek = LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy")).getDayOfWeek().toString().substring(0, 3);
+	        String begin = new SimpleDateFormat("HH:mm:ss").format(event.getDateStart().getValue());
+	        String end = new SimpleDateFormat("HH:mm:ss").format(event.getDateEnd().getValue());
 	        String location = event.getLocation().getValue();
 	        String uc = event.getSummary().getValue().split("-")[0];
-//Done: begin, end, location, inscritos no turno, uc
-	       // Schedule schedule = new Schedule(null, unidadeCurricular, null, null, null, null, begin, end, dataAula, location, null);
-	        Schedule schedule = new Schedule("", uc, turno, "", getInscritosFromDescription(description), dayOfTheWeek, begin, end, date, location, null);
+	        //Done: begin, end, location, inscritos no turno, uc
+	        String scheduleDate = String.format("%s %s", dayOfTheWeek, date);
+	        String formattedBeginTime = String.format("%s:00", begin);
+	        String formattedEndTime = String.format("%s:00", end);
+	        Schedule schedule = new Schedule("", uc, turno, "", getInscritosFromDescription(description), scheduleDate, formattedBeginTime, formattedEndTime, date, location, null);
 	        schedules.add(schedule);
 	    }
-
 	    return schedules;
 	}
 	private static String getInscritosFromDescription(String description) {
